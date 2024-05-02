471b17351e17d0ab869e7b30c134fc5d6b41dd3d
alessandrom10 <alemasini2000@gmail.com>
2024-05-02 12:35:49 +0200

    Added notebooks for static and llm-based dataset normalization

--- LLM_norm_dataset.py
+++ LLM_norm_dataset.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 torch.random.manual_seed(0)
 
-df = pd.read_csv("Datasets/new_dataset.csv", encoding="utf-16", sep="\t")
+#df = pd.read_csv("Datasets/new_dataset.csv", encoding="utf-16", sep="\t")
+df = pd.read_csv("Datasets/claim_review.csv")
 
 # print pd column names
 print(df.columns)
@@ -16,11 +17,13 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
 
-system_prompt = "You are an assisstant that helps fact-checking claims. Given a claim and the label telling its veracity, you need to normalize the label to standart format, between this choices: True, Mostly False, False, Mixture. Answer only with the label."
+system_prompt = "You are an assistant that helps fact-checking claims. Given a claim and the label telling its veracity, you need to normalize the label to standart format, between this choices: True, Mostly False, False, Mixture. Answer only with the label."
 
-a = 100
+#a = 100
+a = 98
 claim = df["claimReviewed"].iloc[a]
-label = df["reviewRating.alternateName"].iloc[a]
+#label = df["reviewRating.alternateName"].iloc[a]
+label = "This statement is totally false"
 
 claim_review = f"Claim: {claim}\nLabel: {label}"
 
@@ -42,5 +45,5 @@ generation_args = {
 }
 
 print(claim_review)
-output = pipe(messages, **generation_args)
-print(output[0]['generated_text'])
+#output = pipe(messages, **generation_args)
+#print(output[0]['generated_text'])
