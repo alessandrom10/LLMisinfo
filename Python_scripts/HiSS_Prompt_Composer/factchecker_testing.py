d163b61cfe0da97d256bad9d9ff56562e2a8c501
alessandrom10 <alemasini2000@gmail.com>
2024-09-22 19:49:47 +0200

    Added accurancy calculator

--- Python_scripts/HiSS_Prompt_Composer/factchecker_testing.py
+++ Python_scripts/HiSS_Prompt_Composer/factchecker_testing.py
@@ -4,6 +4,75 @@ import os
 import re
 import pandas as pd
 
+my_config = load_config("my_config.yaml")
+dataset_input_path = my_config['dataset_input_path']
+dataset_output_path = my_config['dataset_output_path']
+log_file_path = my_config['log_file_path']
+label_column_name = my_config['label_column_name']
+claim_column_name = my_config['claim_column_name']
+date_column_name = my_config['date_column_name']
+author_column_name = my_config['author_column_name']
+possible_labels = my_config['possible_labels']
+labels_values = my_config['labels_values']
+
+if __name__ == "__main__":
+    main()
+
+def main():
+
+    # Load the dataset using pandas
+    dataset = pd.read_csv(dataset_input_path, encoding="utf-16", sep="\t", dtype={24: str})
+    start_index = 0
+    #end_index = 15
+    end_index = dataset.size - 1
+    correct_predictions = 0
+    number_of_claims = 0
+    dataset = dataset.iloc[start_index : end_index]
+    dataset["predicted_label"] = "ERR"
+
+    # Iterate over the rows of the dataset
+    with LoggingPrinter(log_file_path):
+        for index, row in dataset.iterrows():
+            claim = row[claim_column_name]
+            #date = row[date_column_name]
+            #author = row[author_column_name]
+            date = ""
+            author = ""
+            true_label = row[label_column_name]
+            
+            # Call the fact checker function
+            if pd.isna(date):
+                date = ""
+            if pd.isna(author):
+                author = ""
+            user_input = {"claim": claim, "date": date, "author": author}
+
+            for i in range(100):
+                try:
+                    response = generate_output(user_input)
+                    break
+                except Exception as e:
+                    print(f"User_input is: \n{user_input}\n End of user input")
+                    print(f"Error is: \n {e}\n End of error")
+                    print("Pausing for a while and retrying...")
+                    time.sleep(60)
+
+            # Print the predicted label
+            predicted_label = extract_final_answer(response, possible_labels)
+            print(f"Predicted label: {predicted_label}. True label: {true_label}")
+            dataset.at[index, "predicted_label"] = predicted_label
+
+            # Calculate the current accurancy
+            if predicted_label == "ERR":
+                number_of_claims -= 1
+            elif is_similar(predicted_label, true_label.lower()):
+                correct_predictions += 1
+            number_of_claims += 1
+            print(f"Current accurancy is: {correct_predictions / number_of_claims:.3f}")
+
+            # Save the dataset
+            dataset.to_csv(dataset_output_path, index = False, encoding = "utf-16", sep = "\t")
+
 # The class to keep the logs of our operations
 class LoggingPrinter:
     def __init__(self, filename):
@@ -54,60 +123,19 @@ def find_label(model_output, label_list):
             return label
     return "ERR"
 
-def main():
+# This function tells us if the two received labels can be considered as having a similar veracity value
+def is_similar(predicted_label, true_label):
+    if(true_label == "mostly false"):
+        true_label = "mostly-false"
+    if(true_label == "mostly true"):
+        true_label = "mostly-true"
 
-    my_config = load_config("my_config.yaml")
-    dataset_input_path = my_config['dataset_input_path']
-    dataset_output_path = my_config['dataset_output_path']
-    log_file_path = my_config['log_file_path']
-    label_column_name = my_config['label_column_name']
-    claim_column_name = my_config['claim_column_name']
-    date_column_name = my_config['date_column_name']
-    author_column_name = my_config['author_column_name']
-    possible_labels = my_config['possible_labels']
+    label_to_number = {possible_labels[i].lower() : labels_values[i] for i in range(len(possible_labels))}
 
-    # Load the dataset using pandas
-    dataset = pd.read_csv(dataset_input_path, encoding="utf-16", sep="\t", dtype={24: str})
-    start_index = 0
-    end_index = 3
-    #end_index = df.size - 1
-    dataset = dataset.iloc[start_index : end_index]
-    dataset["predicted_label"] = "ERR"
+    mapped_predicted_label = label_to_number[predicted_label]
+    mapped_true_label = label_to_number[true_label]
 
-    # Iterate over the rows of the dataset
-    with LoggingPrinter(log_file_path):
-        for index, row in dataset.iterrows():
-            claim = row[claim_column_name]
-            #date = row[date_column_name]
-            #author = row[author_column_name]
-            date = ""
-            author = ""
-            true_label = row[label_column_name]
-            
-            # Call the fact checker function
-            if pd.isna(date):
-                date = ""
-            if pd.isna(author):
-                author = ""
-            user_input = {"claim": claim, "date": date, "author": author}
-
-            for i in range(100):
-                try:
-                    response = generate_output(user_input)
-                    break
-                except Exception as e:
-                    print(f"User_input is: \n{user_input}\n End of user input")
-                    print(f"Error is: \n {e}\n End of error")
-                    print("Pausing for a while and retrying...")
-                    time.sleep(60)
-
-            # Print the predicted label
-            predicted_label = extract_final_answer(response, possible_labels)
-            print(f"Predicted label: {predicted_label}. True label: {true_label}")
-            dataset.at[index, "predicted_label"] = predicted_label
-
-            # Save the dataset
-            dataset.to_csv(dataset_output_path, index = False, encoding = "utf-16", sep = "\t")
-
-if __name__ == "__main__":
-    main()
+    if(abs(mapped_true_label - mapped_predicted_label) <= 1):
+        return True
+    else:
+        return False
